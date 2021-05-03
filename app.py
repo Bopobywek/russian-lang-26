296d2feb7eea8a564f627385437dc95b4bf9727f
AndrewN <domiantor@yandex.ru>
2021-05-03 14:48:28 +0300

    update files

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import random
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'filesystem'
+with open("tasks.json") as fin:
+       data = json.loads(fin.read())
 
 
 @app.route("/", methods=["GET"])
@@ -14,11 +17,14 @@ def index():
 
 @app.route("/task/<string:last>/", methods=["GET"])
 def get_new_task(last):
-    with open("tasks.json") as fin:
-       data = json.loads(fin.read())
-
-    ch = random.choice(list(data.keys()))
-    while ch == last:
-        ch = random.choice(list(data.keys()))
+    tasks = session.get("tasks", None)
+    if tasks is not None and (tasks.index(last) + 1) != len(tasks):
+        ch = tasks[tasks.index(last) + 1]
+        print(tasks.index(last), len(tasks))
+    else:
+        tasks = list(data.keys())
+        random.shuffle(tasks)
+        ch = tasks[0]
+        session["tasks"] = tasks
     return json.dumps({"task_id": int(ch), "question": list(data[ch].keys())[0].capitalize(), "answer": list(data[ch].values())[0]})
 
